d395533afbea71c37169633e48aeb8a61b361843
3xpl0itz <dfeliton@ucsd.edu>
2022-04-22 11:02:55 -0700

    it works :)

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,10 @@
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class MarkdownParseTest {
@@ -7,6 +12,18 @@ public class MarkdownParseTest {
 	@Test
 	public void addition() {
 		assertEquals(2, 1+1);
+	}
+	
+	@Test
+	public void testMarkdownFileOne() {
+		ArrayList<String> toReturn = new ArrayList<>();
+		toReturn.add("test.html");
+		
+		try {
+			assertEquals(toReturn, MarkdownParse.getLinks(Files.readString(Path.of("/Users/dominicfeliton/Documents/GitHub/markdown-parser/test-file2.md"))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
